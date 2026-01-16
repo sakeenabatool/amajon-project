4b0634d99cabf34c725722f2b746b28731f344b5
sakeenabatool <sakeenabatool358@gmail.com>
2026-01-16 07:59:00 +0500

    feat: Delete from Cart is Interactive

--- data/cart.js
+++ data/cart.js
@@ -1,4 +1,19 @@
-export const cart = [];
+export let cart = [
+  {
+    id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+    quantity: 2,
+  },
+];
+
+export function removeFromCart(productId) {
+  const newCart = [];
+  cart.forEach((item) => {
+    if (item.id !== productId) {
+      newCart.push(item);
+    }
+  });
+  cart = newCart;
+}
 
 export function addToCart(productId) {
   let matchingitem;
